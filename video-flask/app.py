d000ecd9a6dbc5a82ccbc7a560cd7bde22b07149
mlnunes <mlnunes@hotmail.com>
2019-03-08 14:44:07 -0300

    Update app.py

--- video-flask/app.py
+++ video-flask/app.py
@@ -1,12 +1,17 @@
 from flask import Flask
 from flask import render_template
+from os import chdir, listdir
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return render_template('home.html')
-	
+def videos():
+	arquivos = listdir('.\static')
+	mp4 = []
+	for a in arquivos:
+		if a.endswith(".mp4"):
+			mp4.append(a)
+	return render_template('home.html', filmes=mp4)
 
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
